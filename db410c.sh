0e1ba95e2dfbffbc6179abf051d58de6c6258168
Sahaj Sarup <sahaj.sarup@linaro.org>
2019-05-13 18:29:24 +0530

    minor fixes and improvemnts
    - Use zram instead of dd to create swap, this help reduce r/w cycles on the emmc
    - add missing dependency for libssl-dev
    
    Signed-off-by: Sahaj Sarup <sahaj.sarup@linaro.org>

--- db410c.sh
+++ db410c.sh
@@ -35,7 +35,7 @@ GSTREAMER_AUDIO_SINK="alsasink"
 
 install_dependencies() {
   sudo apt-get update
-  sudo apt-get -y install git gcc cmake build-essential libsqlite3-dev libcurl4-openssl-dev libfaad-dev libsoup2.4-dev libgcrypt20-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-good libasound2-dev sox gedit vim python3-pip
+  sudo apt-get -y install git gcc cmake build-essential libsqlite3-dev libcurl4-openssl-dev libfaad-dev libsoup2.4-dev libgcrypt20-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-good libasound2-dev sox gedit vim python3-pip libssl-dev
   # DH - added for DB410c 
   sudo apt-get -y install python-pip
   pip install flask commentjson
